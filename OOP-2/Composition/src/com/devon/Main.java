7d0cf468a70e6541a83b35814c2d76f35753947a
Devon Langendoerfer <425devon@gmail.com>
2018-05-23 15:31:45 -0700

    added computer model with composition example

---
+++ OOP-2/Composition/src/com/devon/Main.java
@@ -0,0 +1,17 @@
+package com.devon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimension dimensions = new Dimension(10,30,14);
+        MotherBoard motherBoard = new MotherBoard("12345","Asus",4,"roksus");
+        Resolution nativeResolution = new Resolution(1920,1200);
+        Monitor monitor = new Monitor("12dw3","Acer",24, nativeResolution);
+	    Case theCase = new Case("22b","Dell","120v",dimensions);
+	    PC myAwesomePC = new PC(theCase,monitor,motherBoard);
+
+	    myAwesomePC.getTheCase().pressPowerButton();
+	    myAwesomePC.getMonitor().drawPixelAt(240,300,"green");
+	    myAwesomePC.getMotherboard().loadProgram("Galaxy Quest");
+    }
+}
---
+++ OOP-2/Composition/src/com/devon/PC.java
@@ -0,0 +1,37 @@
+package com.devon;
+
+public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private MotherBoard motherboard;
+
+    public PC(Case theCase, Monitor monitor, MotherBoard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public void setTheCase(Case theCase) {
+        this.theCase = theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public MotherBoard getMotherboard() {
+        return motherboard;
+    }
+
+    public void setMotherboard(MotherBoard motherboard) {
+        this.motherboard = motherboard;
+    }
+}
