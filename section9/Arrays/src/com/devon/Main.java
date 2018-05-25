3c91fb0c6aa50257e0aa88ce3820033ee136e4b0
Devon Langendoerfer <425devon@gmail.com>
2018-05-25 15:05:10 -0700

    Created directory for section9 exercises

---
+++ section9/Arrays/src/com/devon/Main.java
@@ -0,0 +1,20 @@
+package com.devon;
+
+import com.devon.Models.IntArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        IntArray intArray1 = new IntArray();
+//        intArray1.initArray(10);
+//        intArray1.getArrayLength();
+//        intArray1.insert(0,100);
+//        intArray1.insert(10,45);
+//        System.out.println("************");
+//        IntArray intArray2 = new IntArray();
+//        intArray2.getArrayLength();
+//        intArray2.createRange(10,50);
+//        intArray2.printArray();
+//        intArray2.getArrayLength();
+    }
+}
---
+++ section9/Arrays/src/com/devon/Models/IntArray.java
@@ -0,0 +1,51 @@
+package com.devon.Models;
+
+public class IntArray {
+    private int[] intArray;
+
+    public IntArray() {
+    }
+
+    public void initArray(int size){
+        this.intArray = new int[size];
+    }
+
+    public void getArrayLength(){
+        try{
+            int length = this.intArray.length;
+            System.out.println(length);
+        }catch (NullPointerException e){
+            System.out.println("Array has no length \nplease use initArray()");
+        }
+
+    }
+
+    public int[] getIntArray(){
+        return this.intArray;
+    }
+
+    public void insert(int index, int value){
+        try{
+            intArray[index] = value;
+        }catch (NullPointerException e){
+            System.out.println(e);
+        }
+    }
+
+    public void createRange(int start, int stop){
+        int length = (stop - start);
+        this.initArray(length+1);
+        int count = start;
+        for(int i = 0; i < this.intArray.length; i++){
+            this.intArray[i] = count;
+            count++;
+        }
+    }
+
+    public void printArray(){
+        for(int val: this.intArray){
+            System.out.println(val);
+        }
+    }
+
+}
