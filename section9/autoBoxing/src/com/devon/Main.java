ecd830399878c858230ad000ce7c559a5951dd51
Devon Langendoerfer <425devon@gmail.com>
2018-05-31 20:29:55 -0700

    added autoBoxing -unBoxing examples

--- section9/autoBoxing/src/com/devon/Main.java
+++ section9/autoBoxing/src/com/devon/Main.java
@@ -1,8 +1,35 @@
 package com.devon;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Integer integer = new Integer(54);
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+//        //Old Deprecated
+//        for(int i = 0; i< 10; i++){
+//            intList.add(Integer.valueOf(i));
+//        }
+//        for(int i : intList){
+//            System.out.println(intList.get(i).intValue());
+//        }
+
+        for(int i = 0; i< 10; i++){
+            intList.add(i);
+        }
+        for(int i : intList){
+            System.out.println(intList.get(i));
+        }
+
+        ArrayList<Double> dblMint = new ArrayList<Double>();
+        for(Double d=0.0; d<=10; d+=0.5){
+            dblMint.add(d);
+        }
+        for(Double d : dblMint){
+            System.out.println(d);
+        }
     }
 }
