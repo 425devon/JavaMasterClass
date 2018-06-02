c7e5dbf143b05c6b6ab3b2dfbe4c228f3e7bb280
Devon Langendoerfer <425devon@gmail.com>
2018-06-02 10:26:14 -0700

    added linked list and listIterator examples

--- section9/linkedList/src/com/devon/Main.java
+++ section9/linkedList/src/com/devon/Main.java
@@ -1,8 +1,48 @@
 package com.devon;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedList<String> cities = new LinkedList<String>();
+       addInAlpha(cities, "Chicago");
+        addInAlpha(cities, "Albany");
+        addInAlpha(cities, "Dallas");
+        addInAlpha(cities, "Cairo");
+        addInAlpha(cities, "LA");
+
+        printList(cities);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+        while (i.hasNext()) {
+            System.out.println("Now traveling to to " + i.next());
+        }
+        System.out.println("--------------");
+    }
+
+    private static boolean addInAlpha(LinkedList<String> linkedList, String newCity){
+        ListIterator<String> i = linkedList.listIterator();
+
+        while(i.hasNext()){
+            int comparison = i.next().compareTo(newCity);
+            if(comparison == 0){
+                //equal, do not add.
+                return false;
+            }else if(comparison > 0){
+                //new city should appear before this one...
+                i.previous();
+                i.add(newCity);
+                return true;
+            }else if(comparison < 0){
+                //move onto next city
+            }
+        }
+        i.add(newCity);
+        return true;
     }
 }
